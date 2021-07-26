9814a88081a9868ab02ff3150450db79bbe86bc8
Arber Hajrizaj <ah44219@ubt-uni.net>
2021-07-27 00:58:35 +0200

    Header Created
    Included animations,Sticky menu etc

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 function videoUrl(hm){
     document.getElementById("slider1").src = hm;
 }
+
+$(window).scroll(function() {
+	if($(document).scrollTop() < 1){
+		$('.header').removeClass('fixed');
+		$('.header').removeClass('active');		
+	}else if($(document).scrollTop() > 100 ){
+		$('.header').removeClass('fixed');
+		$('.header').addClass('active');
+	}else{
+		$('.header').addClass('fixed');
+	}
+});
